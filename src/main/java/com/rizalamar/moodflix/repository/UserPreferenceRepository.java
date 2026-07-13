6fef005136c5699d96e5e18b70a1bdab079e57cc
Rizal amarulloh <rizalamarulloh2014@gmail.com>
2026-07-13 13:39:39 +0700

    feat: implement repository interfaces

--- src/main/java/com/rizalamar/moodflix/repository/UserPreferenceRepository.java
+++ src/main/java/com/rizalamar/moodflix/repository/UserPreferenceRepository.java
@@ -1,4 +1,11 @@
 package com.rizalamar.moodflix.repository;
 
-public interface UserPreferenceRepository {
+import com.rizalamar.moodflix.domain.UserPreference;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserPreferenceRepository extends JpaRepository<UserPreference, UUID> {
 }
