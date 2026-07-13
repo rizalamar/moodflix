1bc72f79a8cf6f445a3259f8b03e97ea495d28b5
Rizal amarulloh <rizalamarulloh2014@gmail.com>
2026-07-13 20:46:44 +0700

    feat: implement CustomUserDetailsService - Connect Spring Security to database via Userrepository - Implement UserDetailService to load UserPrincipal during authentication - Add read-only transaction support for optimized data loading

--- src/main/java/com/rizalamar/moodflix/security/CustomUserDetailsService.java
+++ src/main/java/com/rizalamar/moodflix/security/CustomUserDetailsService.java
@@ -1,4 +1,26 @@
 package com.rizalamar.moodflix.security;
 
-public class CustomUserDetailsService {
+import com.rizalamar.moodflix.domain.User;
+import com.rizalamar.moodflix.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+
+        return UserPrincipal.create(user);
+    }
 }
